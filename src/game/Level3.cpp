8bf0ca0fa26ca061d9d30c36b4e468c1efd4e086
koksneo <Dawid@.(none)>
2010-12-31 18:03:16 +0100

    Resolve compile errors

--- src/game/Level3.cpp
+++ src/game/Level3.cpp
@@ -6799,7 +6799,7 @@ bool ChatHandler::HandleMmapTestArea(char* args)
         PSendSysMessage("Found %i Creatures.", creatureList.size());
 
         uint32 paths = 0;
-        uint32 uStartTime = getMSTime();
+        uint32 uStartTime = WorldTimer::getMSTime();
 
         float gx,gy,gz;
         m_session->GetPlayer()->GetPosition(gx,gy,gz);
@@ -6809,7 +6809,7 @@ bool ChatHandler::HandleMmapTestArea(char* args)
             ++paths;
         }
 
-        uint32 uPathLoadTime = getMSTimeDiff(uStartTime, getMSTime());
+        uint32 uPathLoadTime = WorldTimer::getMSTimeDiff(uStartTime, WorldTimer::getMSTime());
         PSendSysMessage("Generated %i paths in %i ms", paths, uPathLoadTime);
     }
     else
