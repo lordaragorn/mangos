776d77cb59aad4ae3bdc4afb336cde6418962d2c
PanteraPolnocy <NorthPanther@interia.pl>
2011-01-08 02:35:34 +0100

    Revert "Merge branch '335' of github.com:przemratajczak/mangos into update"
    This reverts commit 1fc8ad759348a6b6fa3e057203d4100f5a4ef4a3, reversing
    changes made to 28375bbc62efe7a2a54d6bc046975733cfdbd5b0.

--- src/bindings/ScriptDev2/scripts/northrend/nexus/eye_of_eternity/boss_malygos.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/nexus/eye_of_eternity/boss_malygos.cpp
@@ -554,6 +554,7 @@ struct MANGOS_DLL_DECL boss_malygosAI : public ScriptedAI
                 pTempVortexVisual->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NON_ATTACKABLE);
                 pTempVortexVisual->RemoveFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE);
                 pTempVortexVisual->setFaction(85);
+                m_creature->AddThreat(pTempVortexVisual, 1000000000.0f);
 
                 pTempVortexVisual->CastSpell(pTempVortexVisual, SPELL_VORTEX_VISUAL, true);
             }
@@ -686,12 +687,8 @@ struct MANGOS_DLL_DECL boss_malygosAI : public ScriptedAI
             SetCombatMovement(true);
             DoCast(m_creature, SPELL_BERSERK, true);
             m_uiEnrageTimer = 600000;
-
-            if (Unit *pVictim = m_creature->getVictim())
-            {
-                m_creature->GetMotionMaster()->Clear(false, true);
-                m_creature->GetMotionMaster()->MoveChase(pVictim);
-            }
+            m_creature->GetMotionMaster()->Clear(false, true);
+            m_creature->GetMotionMaster()->MoveChase(m_creature->getVictim());
         }
         else
             m_uiEnrageTimer -= uiDiff;
