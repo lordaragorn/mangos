956a8f641802cf9e5e498a9d004bf3f94cd765ff
michalpolko <michalpolko@o2.pl>
2011-03-27 14:39:44 +0200

    [a643] Script for Hadronox (Azjol Nerub).

--- src/bindings/ScriptDev2/scripts/northrend/azjol-nerub/azjol-nerub/boss_hadronox.cpp
+++ src/bindings/ScriptDev2/scripts/northrend/azjol-nerub/azjol-nerub/boss_hadronox.cpp
@@ -16,7 +16,7 @@
 
 /* ScriptData
 SDName: Boss_Hadronox
-SD%Complete: 20%
+SD%Complete: 
 SDComment:
 SDCategory: Azjol'Nerub
 EndScriptData */
@@ -26,6 +26,20 @@ EndScriptData */
 
 enum
 {
+    SPELL_ACID_CLOUD            = 53400,
+    SPELL_ACID_CLOUD_H          = 59419,
+    SPELL_LEECH_POISON          = 53030,
+    SPELL_LEECH_POISON_H        = 59417,
+    SPELL_PIERCE_ARMOR          = 53418,
+    SPELL_WEB_GRAB              = 53406,
+    SPELL_WEB_GRAB_H            = 59420,
+    SPELL_WEB_FRONT_DOORS       = 53177, // dummy effect, let's players know that no more adds are spawning
+    SPELL_WEB_SIDE_DOORS        = 53185,
+
+
+    MOB_NECROMANCER             = 29098,
+    MOB_CHAMPION                = 29117,
+    MOB_CRYPT_FRIEND            = 29063,
 
 };
 
@@ -45,8 +59,33 @@ struct MANGOS_DLL_DECL boss_hadronoxAI : public ScriptedAI
     instance_azjol_nerub* m_pInstance;
     bool m_bIsRegularMode;
 
+    uint32 m_uiAcidCloudTimer;
+    uint32 m_uiLeechPoisonTimer;
+    uint32 m_uiPierceArmorTimer;
+    uint32 m_uiWebGrabTimer;
+    uint32 m_uiWebFrontDoorsTimer;
+    uint32 m_uiWebSideDoorsTimer;
+
     void Reset()
     {
+        m_uiAcidCloudTimer      = 20000;
+        m_uiLeechPoisonTimer    = 9000;
+        m_uiPierceArmorTimer    = 8000;
+        m_uiWebGrabTimer        = 24000;
+        m_uiWebFrontDoorsTimer  = 50000;
+        m_uiWebSideDoorsTimer   = 49000;
+    }
+
+    void Aggro(Unit *pWho)
+    {
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_HADRONOX, IN_PROGRESS);
+    }
+
+    void JustDied(Unit *pKiller)
+    {
+        if (m_pInstance)
+            m_pInstance->SetData(TYPE_HADRONOX, DONE);
     }
 
     void KilledUnit(Unit* pVictim)
@@ -54,10 +93,58 @@ struct MANGOS_DLL_DECL boss_hadronoxAI : public ScriptedAI
         m_creature->SetHealth(m_creature->GetHealth() + (m_creature->GetMaxHealth() * 0.1));
     }
 
+
     void UpdateAI(const uint32 uiDiff)
     {
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
+
+        if (m_uiAcidCloudTimer <= uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+            {
+                DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_ACID_CLOUD : SPELL_ACID_CLOUD_H);
+                m_uiAcidCloudTimer = 20000;
+            }
+        }else m_uiAcidCloudTimer -= uiDiff;
+
+        if (m_uiLeechPoisonTimer <= uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+            {
+                DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_LEECH_POISON : SPELL_LEECH_POISON_H);
+                m_uiLeechPoisonTimer = 9000;
+            }
+        }else m_uiLeechPoisonTimer -= uiDiff;
+
+        if (m_uiPierceArmorTimer <= uiDiff)
+        {
+            DoCastSpellIfCan(m_creature->getVictim(), SPELL_PIERCE_ARMOR);
+            m_uiPierceArmorTimer = 8000;
+        }else m_uiPierceArmorTimer -= uiDiff;
+
+        if (m_uiWebGrabTimer <= uiDiff)
+        {
+            if (Unit* pTarget = m_creature->SelectAttackingTarget(ATTACKING_TARGET_RANDOM, 0))
+            {
+                DoCastSpellIfCan(pTarget, m_bIsRegularMode ? SPELL_WEB_GRAB : SPELL_WEB_GRAB_H);
+                m_uiWebGrabTimer = 24000;
+            }
+        }else m_uiWebGrabTimer -= uiDiff;
+
+        /*if (m_uiWebFrontDoorsTimer <= uiDiff)
+        {
+            DoCastSpellIfCan(m_creature, SPELL_WEB_FRONT_DOORS);
+            m_uiWebFrontDoorsTimer = 50000;
+        }else m_uiWebFrontDoorsTimer -= uiDiff;
+
+
+        if (m_uiWebSideDoorsTimer <= uiDiff)
+        {
+            DoCastSpellIfCan(m_creature, SPELL_WEB_SIDE_DOORS);
+            m_uiWebSideDoorsTimer = 49000;
+        }else m_uiWebSideDoorsTimer -= uiDiff;*/
+
 
         DoMeleeAttackIfReady();
     }
